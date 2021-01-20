b2b9d156169cbee4c2cd3d930de1a01e8d659186
Lamobot <valery@toropow.ru>
2021-01-20 17:22:39 +0300

    MySQL connection has been refactored

--- libs.py
+++ libs.py
@@ -4,8 +4,6 @@
 import logging
 import requests
 from bs4 import BeautifulSoup
-import mysql.connector
-import config
 
 logging.basicConfig(filename='script.log', filemode='w', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(funcName)s:  %(message)s')
@@ -33,55 +31,3 @@ def get_url_response(url: str) -> object:
     if response.status_code == 200:
         return BeautifulSoup(response.text, "html.parser")
     return None
-
-
-def get_links_from_database(category: tuple) -> list:
-    """
-    Return list of strings with urls for parsing
-    :param category: one of this tuples:
-        - ("globus products", )
-    :return: list of urls
-    """
-    url_list = []
-    conn = mysql.connector.connect(
-        host=config.db_server,
-        user=config.db_user,
-        password=config.db_password,
-        database=config.db_name
-    )
-    curs = conn.cursor()
-
-    sql = """
-        SELECT link FROM links l
-        JOIN categories c ON l.categories_id = c.id
-        WHERE c.name = %s
-    """
-    curs.execute(sql, category)
-    result = curs.fetchall()
-    for link in result:
-        url_list.append(link[0])
-    conn.close()
-    return url_list
-
-
-def insert_calculated_index_to_database(calculated_index: tuple) -> None:
-    """
-    This function inserts calculated index to database
-    :param calculated_index: Tuple of calculated indexes
-    :return: None
-    """
-    conn = mysql.connector.connect(
-        host=config.db_server,
-        user=config.db_user,
-        password=config.db_password,
-        database=config.db_name
-    )
-    curs = conn.cursor()
-
-    sql = """
-        INSERT INTO indexes (transportation_index, housing_sector_index, product_index)
-        VALUES (%s, %s, %s)
-        """
-    curs.execute(sql, calculated_index)
-    conn.commit()
-    conn.close()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from libs import get_links_from_database, get_url_response, insert_calculated_index_to_database
+# -*- coding: utf-8 -*-
+"""Main module"""
+
+from libs import get_url_response
 from fuel_prices import get_fuel_price, get_mcd_ticket_price
 from products import get_product_index_from_globus, get_product_index_from_vprok
 from housing_sector import get_housing_service_price, get_electricity_price
+from classes import ConnectToDatabase
 
 FILENAME = 'tmp/bill.pdf'
 URL_TO_PARSE_FUEL = "https://auto.mail.ru/fuel/"
@@ -10,19 +14,29 @@ URL_TO_PARSE_ELECTRICITY_PRICE = "https://www.mosenergosbyt.ru/individuals/tarif
                                  "tariffs-mo/kvartiry-i-doma-s-elektricheskimi-plitami-mo.php"
 
 
-def calculate_index() -> tuple:
-    product_index = (get_product_index_from_globus(get_links_from_database(("globus products",))) +
-                     get_product_index_from_vprok(get_links_from_database(("vprok products",)))) / 2
+def main() -> tuple:
+    """
+    This function calculates indexes
+    :return: tuple of product, fuel and housing sector index
+    """
+    product_index = (
+        get_product_index_from_globus(
+            conn.get_links_from_database(("globus products",))
+            ) + get_product_index_from_vprok(
+            conn.get_links_from_database(("vprok products",)))) / 2
 
     fuel_index = get_fuel_price(get_url_response(URL_TO_PARSE_FUEL), 'ai95') + \
         get_fuel_price(get_url_response(URL_TO_PARSE_FUEL), 'dt') + \
         get_mcd_ticket_price(get_url_response(URL_TO_PARSE_MCD_TICKETS))
 
-    housing_sector_index = get_electricity_price(get_url_response(URL_TO_PARSE_ELECTRICITY_PRICE)) + \
-        get_housing_service_price(FILENAME)
+    housing_sector_index = get_electricity_price(
+        get_url_response(URL_TO_PARSE_ELECTRICITY_PRICE)
+    ) + get_housing_service_price(FILENAME)
 
-    return product_index, fuel_index, housing_sector_index,
+    return product_index, fuel_index, housing_sector_index
 
 
 if __name__ == "__main__":
-    insert_calculated_index_to_database(calculate_index())
+    conn = ConnectToDatabase()
+    conn.insert_calculated_index_to_database(main())
+
