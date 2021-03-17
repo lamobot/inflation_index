c66f9289227d7091d029bff46164d902a8e6d69b
Valery Toropov <valery@toropow.ru>
2021-03-17 21:21:29 +0300

    Config has been pushed to git

---
+++ config.py
@@ -0,0 +1,9 @@
+# Constants
+FILENAME = 'tmp/bill.pdf'
+URL_TO_PARSE_FUEL = "https://auto.mail.ru/fuel/"
+URL_TO_PARSE_MCD_TICKETS = "https://troikarta.ru/tarify/mcd/"
+URL_TO_PARSE_ELECTRICITY_PRICE = "https://www.mosenergosbyt.ru/individuals/tariffs-n-payments/" \
+                                 "tariffs-mo/kvartiry-i-doma-s-elektricheskimi-plitami-mo.php"
+
+FUEL_COUNTER_LITTERS_PER_MONTH: int = 40
+TRAIN_TICKETS_COUNTER_PER_MONTH: int = 41
